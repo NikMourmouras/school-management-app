93ae53af5156f2f298b480e77ebce26f00560a22
Nik Mourmouras <104062602+NikMourmouras@users.noreply.github.com>
2025-07-05 16:23:09 +0300

    Set up MainMenuFrame as initial window

--- src/GradeDAO.java
+++ src/GradeDAO.java
@@ -1,6 +1,9 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GradeDAO {
 
@@ -26,6 +29,39 @@ public class GradeDAO {
         }
 
     }
+
+
+    public List<Grade> getGradesByStudentId(int studentId) {
+        List<Grade> grades = new ArrayList<>();
+
+        String sql = "SELECT g.grade, s.id AS subject_id, s.title " +
+                "FROM grades g " +
+                "JOIN subjects s ON g.subject_id = s.id " +
+                "WHERE g.student_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, studentId);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                double gradeValue = resultSet.getDouble("grade");
+                int subjectId = resultSet.getInt("subject_id");
+                String subjectTitle = resultSet.getString("title");
+
+                Subject subject = new Subject(subjectId, subjectTitle);
+                Grade grade = new Grade(gradeValue, null, subject); // student μπορεί να είναι null εδώ
+
+                grades.add(grade);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("❌ Error fetching grades: " + e.getMessage());
+        }
+
+        return grades;
+    }
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,8 @@
-import java.sql.Connection;
-import java.sql.SQLException;
+import javax.swing.*;
+
 
 public class Main {
     public static void main(String[] args){
-
-        DBUtils dbUtils = new DBUtils();
-//        Connection connection = null;
-//        try {
-//            connection = dbUtils.geConnection(dbUtils.url, dbUtils.user, dbUtils.password);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        System.out.println(connection);
-
-
-        Connection connection = DBUtils.getConnection();
-//        StudentDAO studentDAO = new StudentDAO(connection);
-//        SubjectDAO subjectDAO = new SubjectDAO(connection);
-
-//        Student student = new Student("Maria", "Papadopoulou", "10-03-1991", "Female", "Athens");
-
-//        Subject subject = new Subject("History");
-//        boolean success = subjectDAO.addSubject(subject);
-//
-//
-//
-//        System.out.println(success ? "✅ Subject added!" : "❌ Failed to add subject.");
-
-//        new AddSubjectForm();
-
-//        new AddStudentForm();
-        
-//        new AssignSubjectToStudentForm();
-
-
-//        new AddGradeForm();
-
-
+        SwingUtilities.invokeLater(MainMenuFrame::new);
     }
-
 }
